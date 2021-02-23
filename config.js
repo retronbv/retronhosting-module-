7c34c0edd6df5b8ae7f7f80eb0992f1e9d92e271
HAACKKKER MAAANNNNNN <retronbv@gmail.com>
2021-02-23 12:53:13 +0000

    update

---
+++ config.js
@@ -0,0 +1,10 @@
+{
+  "name": "Test module for retronhosting",
+  "author": "retronbv",
+  "git":"https://gitub.com/retronbv/retronhosting-module-",
+  "config": {
+    "pages": {
+      "/test_module":"modules/retronbv/retronhosting-module-/index.html"
+    }
+  }
+}
